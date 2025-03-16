9464e93375d139feefb870dc108d9ce5dfeb918e
Shahin Moyshan <freelance.shain@gmail.com>
2025-03-16 12:58:26 +0600

    added basic layout

---
+++ resources/views/layouts/includes/header.blade.php
@@ -0,0 +1,21 @@
+<header class="fixed inset-x-0 top-0 bg-gray-50 w-full h-16 md:h-20">
+    <div class="max-w-6xl h-full mx-auto flex flex-col justify-center items-center">
+        <div class="grid grid-cols-7 gap-4 w-full">
+
+            <div class="col-span-2 flex flex-col justify-center">
+                {logo}
+            </div>
+
+            <div class="col-span-3">
+                {search box}
+            </div>
+
+            <div class="col-span-2 flex flex-col justify-center items-end">
+                {cart}
+            </div>
+        </div>
+    </div>
+</header>
+
+{{-- Header Spacer --}}
+<div class="h-16 md:h-20"></div>
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Mini Cart</title>
+
+    <!-- Google Fonts / Inter -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
+        rel="stylesheet">
+
+    <!-- Styles / Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body x-data="miniCart" class="max-w-6xl mx-auto px-4 text-gray-950">
+
+    {{-- Include Header Part --}}
+    @include('layouts.includes.header')
+
+    {{-- Main Content --}}
+    @yield('content')
+
+    {{-- Include Footer Part --}}
+    @include('layouts.includes.footer')
+
+</body>
+
+</html>
